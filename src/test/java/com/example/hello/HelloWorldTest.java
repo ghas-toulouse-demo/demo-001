d69be250caf4b86a534e0f73d7aa5b0b6d278dad
Tugdual Grall <tgrall@github.com>
2025-08-28 17:30:35 +0200

    Add documentation comments for test classes in the hello-lib

--- src/test/java/com/example/hello/HelloWorldTest.java
+++ src/test/java/com/example/hello/HelloWorldTest.java
@@ -3,6 +3,9 @@ package com.example.hello;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+ * Test class for the HelloWorld
+ */
 public class HelloWorldTest {
     @Test
     public void testHello() {
